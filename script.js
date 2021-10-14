4e794bff975ffb49aae924104f034218c2e978f3
Tobi Banks <tobibanks@yahoo.co.uk>
2021-10-14 18:35:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function add (a,b) {
+  return a + b; 
+}
+
+function minus(a,b) {
+  return a - b;
+}
+
+function multiply (a,b) {
+  return a * b;
+}
+
+function divide (a,b) {
+  return a / b;
+}
+
+function operate (operator,num1, num2) {
+  if(operator == (num1 + num2)) {
+    return add();
+  } else if(operator == (num1 - num2)) {
+    return minus();
+  } else if(operator == (num1 * num2)) {
+    return multiply();
+  } else if(operator == (num1 / num2)) {
+    return divide();
+  }
+}
+
+const pressNum = document.querySelectorAll('.btn-num');
+const display_val = document.querySelector('.calc-screen-container-present');
+
+// display numbers on screen
+function displayToScreen() {
+  let display = display_val.textContent += 0;
+  pressNum.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      if(display) {
+        display.textContent += btn.innerHTML;
+      } else if (!display) {
+          display_val.textContent = btn.innerHTML;
+        }
+    }); 
+  });
+} 
+
+displayToScreen();
